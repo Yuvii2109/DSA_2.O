fd645b3fdc318ad276570d6859ab7c30841a4ebe
Yuvraj Sachdeva <yuvrajsach@gmail.com>
2025-05-11 17:06:55 +0530

    Classes

--- Classes/Day - 21/Array2D.java
+++ Classes/Day - 21/Array2D.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
 
 public class Array2D {
+    public static void display(int[][] arr){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[0].length; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         try(Scanner sc = new Scanner(System.in)){
             System.out.print("Enter number of rows - ");
@@ -15,12 +23,7 @@ public class Array2D {
                 }
             }
             System.out.println("\n2D Array -");
-            for(int i = 0; i < arr.length; i++){
-                for(int j = 0; j < arr[0].length; j++){
-                    System.out.print(arr[i][j] + " ");
-                }
-                System.out.println();
-            }
+            display(arr);
         }
     }
 }
