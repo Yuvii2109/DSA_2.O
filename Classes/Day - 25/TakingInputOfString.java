f460b54bf2651454dea9bc1d550d70761bb124cc
Yuvraj Sachdeva <yuvrajsach@gmail.com>
2025-05-25 17:02:52 +0530

    Classes

--- Classes/Day - 25/TakingInputOfString.java
+++ Classes/Day - 25/TakingInputOfString.java
@@ -4,6 +4,9 @@ public class TakingInputOfString {
     public static void main(String[] args) {
         try(Scanner sc = new Scanner(System.in)){
             System.out.print("Enter a String - ");
+            // String k = sc.next();
+            // System.out.println(k);
+            // Iske baad input lena hi band krdega yeh
             String y = sc.nextLine();
             System.out.println("Hi, " + y);
         }
