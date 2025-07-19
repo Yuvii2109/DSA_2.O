0e77ef454b85c0c6f56e73ae1e3331cfbe42d932
Yuvraj Sachdeva <yuvrajsach@gmail.com>
2025-07-19 19:00:16 +0530

    Classes

--- Classes/Day - 34/MergeSort.java
+++ Classes/Day - 34/MergeSort.java
@@ -1,35 +1,42 @@
 public class MergeSort {
-    public static void mergeIntoSorted(int[] arr1, int[] arr2){
-        int i = 0, j = 0, z = 0;
-        int n = arr1.length, m = arr2.length;
-        int[] merged = new int[n + m];
-        while(i < n && j < m){
-            if(arr1[i] < arr2[j]){
-                merged[z] = arr1[i];
-                i++;
+    public static int[] merge(int[] left, int[] right){
+        int[] merged = new int[left.length + right.length];
+        int leftIndex = 0, rightIndex = 0, mergedIndex = 0;
+        while(leftIndex < left.length && rightIndex < right.length){
+            if(left[leftIndex] <= right[rightIndex]){
+                merged[mergedIndex++] = left[leftIndex++];
             }else{
-                merged[z] = arr2[j];
-                j++;
+                merged[mergedIndex++] = right[rightIndex++];
             }
-            z++;
         }
-        while(i < n){
-            merged[z] = arr1[i];
-            i++;
-            z++;
+        while(leftIndex < left.length){
+            merged[mergedIndex++] = left[leftIndex++];
         }
-        while(j < m){
-            merged[z] = arr2[j];
-            j++;
-            z++;
+        while(rightIndex < right.length){
+            merged[mergedIndex++] = right[rightIndex++];
         }
-        for(int k = 0; k < merged.length; k++){
-            System.out.print(merged[k] + " ");
-        }
+        return merged;
     }
-    public static void main(String[] args) {
-        int arr1[] = {1, 7, 8, 9, 10, 12};
-        int arr2[] = {2, 5, 10, 15, 17, 18};
-        mergeIntoSorted(arr1, arr2);
+    public static int[] mergeSort(int[] arr){
+        if(arr.length <= 1){
+            return arr;
+        }
+        int mid = arr.length / 2;
+        int[] left = new int[mid];
+        int[] right = new int[arr.length - mid];
+        for(int i = 0; i < mid; i++){
+            left[i] = arr[i];
+        }
+        for(int i = mid; i < arr.length; i++){
+            right[i - mid] = arr[i];
+        }
+        return merge(mergeSort(left), mergeSort(right));
+    }
+    public static void main(String args[]){
+        int[] arr = {5, 2, 8, 1, 9, 3, 7, 6, 4};
+        int[] sortedArr = mergeSort(arr);
+        for(int i = 0; i < sortedArr.length; i++){
+            System.out.print(sortedArr[i] + " ");
+        }
     }
 }
---
+++ Classes/Day - 34/MergeSortKaKhayal.java
@@ -0,0 +1,35 @@
+public class MergeSortKaKhayal {
+    public static void mergeIntoSorted(int[] arr1, int[] arr2){
+        int i = 0, j = 0, z = 0;
+        int n = arr1.length, m = arr2.length;
+        int[] merged = new int[n + m];
+        while(i < n && j < m){
+            if(arr1[i] < arr2[j]){
+                merged[z] = arr1[i];
+                i++;
+            }else{
+                merged[z] = arr2[j];
+                j++;
+            }
+            z++;
+        }
+        while(i < n){
+            merged[z] = arr1[i];
+            i++;
+            z++;
+        }
+        while(j < m){
+            merged[z] = arr2[j];
+            j++;
+            z++;
+        }
+        for(int k = 0; k < merged.length; k++){
+            System.out.print(merged[k] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr1[] = {1, 7, 8, 9, 10, 12};
+        int arr2[] = {2, 5, 10, 15, 17, 18};
+        mergeIntoSorted(arr1, arr2);
+    }
+}
