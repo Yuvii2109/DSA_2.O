4e597918402b3d8b5539944f7294a3e9b78bccad
Yuvraj Sachdeva <yuvrajsach@gmail.com>
2025-08-05 16:53:46 +0530

    Self Assessment

---
+++ Contests/Contest - 06/Question01.java
@@ -0,0 +1,46 @@
+/*
+    Take as input N, a number. Print odd numbers in decreasing sequence (up until 0) and even numbers in increasing sequence (up until N) using Recursion
+
+    Constraints - 1 <= N <=1000
+
+    Sample Input - 5
+    Sample Output - 
+        5
+        3
+        1
+        2
+        4
+*/
+
+import java.util.Scanner;
+
+public class Question01 {
+    public static void printEven(int n, int i){
+        if(i > n){
+            return;
+        }
+        System.out.println(i);
+        printEven(n, i + 2);
+    }
+    public static void printOdd(int n, int i){
+        if(i <= 0){
+            return;
+        }
+        System.out.println(i);
+        printOdd(n, i - 2);
+    }
+    public static void printOddEven(int n){
+        if(n % 2 == 0){
+            printOdd(n, n - 1);
+        }else{
+            printOdd(n, n);
+        }
+        printEven(n, 2);
+    }
+    public static void main(String args[]) {
+        try(Scanner sc = new Scanner(System.in)){
+            int n = sc.nextInt();
+            printOddEven(n);
+        }
+    }
+}
